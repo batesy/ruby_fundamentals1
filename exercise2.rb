970c64378415fa1d6b3ac4fb5a04fa3744218ac5
batesy <gbatesy@gmail.com>
2013-10-22 14:18:14 -0400

    Completed exercise 2. Asks for name in Exercise 3.

---
+++ exercise2.rb
@@ -0,0 +1,13 @@
+# Calculating a 15% tip on a $55 bill
+meal = 55
+tip = meal * 0.15
+puts tip
+
+# Adding a string and an integer
+puts "Deuce Deuce : " + 22.to_s
+
+# Multiplication inside a sentence using string interpolation
+puts "45628 multiplied by 7839 equals: #{45628 * 7839}."
+
+# The answer is going to be true, because the last boolean will return true.
+puts (true && false) || (false && true) || !(false && false)
