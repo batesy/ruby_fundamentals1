5ae838d352646e8d2b3912e8e35f01fb5b732cba
batesy <gbatesy@gmail.com>
2013-10-22 15:12:18 -0400

    Outputs users birth year.

--- exercise3.rb
+++ exercise3.rb
@@ -2,3 +2,10 @@ puts "What is your name?"
 name = gets.chomp
 
 puts "Well hello there, #{name}!"
+
+puts "How old are you?"
+age = gets.chomp.to_i
+now = Time.now.year
+born = now - age
+
+puts "#{name}, you were born in #{born}."
